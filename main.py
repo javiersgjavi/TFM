fbefaea7c68ff37469cff2f069b24758f24e553f
javiersgjavi <javiersgjavi@gmail.com>
2022-05-22 18:04:21 +0200

    Main file added

---
+++ main.py
@@ -0,0 +1,23 @@
+import argparse
+from Taxi import Taxi
+
+
+def main(args):
+    env_id = int(args.environment[0])
+    if env_id == 1:
+        experiment = Taxi()
+        experiment.train()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-e",
+        "--environment",
+        nargs=1,
+        default=['0'],
+        choices=['0', '1'],
+        help="Environment to use"
+    )
+    args = parser.parse_args()
+    main(args)
